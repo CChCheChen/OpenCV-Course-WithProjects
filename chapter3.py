d8b10c674f6ae8e76b2db630d42ff23ee16902b6
CChCheChen <chen.lin.0404@gmail.com>
2021-12-12 08:35:00 -0500

    resize and crop

--- chapter3.py
+++ chapter3.py
@@ -1,30 +1,17 @@
 import cv2 as cv
 import numpy as np
 
-kernel = np.ones((5, 5), np.uint8)
+# kernel = np.ones((5, 5), np.uint8)
 
 img = cv.imread("Resources/fred.png")
-# cv.imshow("Fred", img)
+cv.imshow("Fred", img)
+# print(img.shape) # resulting (480, 852, 3) where 480 is height and  852 is width, 3 is color channel for BGR
 
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-# cv.imshow("Fred Gray", img_gray)
+img_resize = cv.resize(img, (img.shape[1]*2, img.shape[0]*2)) # (width, height)
+# cv.imshow("Fred resized", img_resize)
+# print(img_resize.shape) # resulting (960, 1704, 3)
 
-img_blur = cv.GaussianBlur(img_gray, (7, 7), 0)
-# cv.imshow("Fred Blur", img_blur)
-
-img_canny1 = cv.Canny(img, 100, 100)
-# cv.imshow("Fred Canny 1", img_canny1)
-img_canny2 = cv.Canny(img, 150, 200)
-# cv.imshow("Fred Canny 2", img_canny2)
-
-img_dilation1 = cv.dilate(img_canny1, kernel, iterations=1)
-# cv.imshow("Fred Dilation 1", img_dilation1)
-img_dilation2 = cv.dilate(img_canny1, kernel, iterations=5)
-# cv.imshow("Fred Dilation 2", img_dilation2)
-
-img_eroded1 = cv.erode(img_dilation1, kernel, iterations=1)
-cv.imshow("Fred Eroded 1", img_eroded1)
-img_eroded2 = cv.erode(img_dilation1, kernel, iterations=0)
-cv.imshow("Fred Eroded 2", img_eroded2)
+img_cropped = img[img.shape[0]//4:3*(img.shape[0]//4), img.shape[1]//4:3*(img.shape[1]//4)] # (height, width)
+cv.imshow("Fred cropped", img_cropped)
 
 cv.waitKey(0)
